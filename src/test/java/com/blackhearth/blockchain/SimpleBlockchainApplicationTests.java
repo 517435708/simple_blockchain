71ad63b29cd270f364d67e98356b3c8ba8e97fb4
517435708 <lphusarz@gmail.com>
2020-10-10 15:15:27 +0200

    init commit

---
+++ src/test/java/com/blackhearth/blockchain/SimpleBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackhearth.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
