ceb7d49e8b4ea87e9130da47928f1f092c2e00a1
517435708 <a500573435708@gmail.com>
2020-10-12 09:15:17 +0200

    block miner with block example

---
+++ src/main/java/com/blackhearth/blockchain/block/Block.java
@@ -0,0 +1,44 @@
+package com.blackhearth.blockchain.block;
+
+
+import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.security.MessageDigest;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Data
+public class Block {
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    Block(String data, String previousHash, long timeStamp) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+        this.hash = calculateBlockHash();
+    }
+
+    @SneakyThrows
+    public String calculateBlockHash() {
+        String dataToHash = previousHash
+                + timeStamp
+                + nonce
+                + data;
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = digest.digest(dataToHash.getBytes(UTF_8));
+        StringBuilder buffer = new StringBuilder();
+        for (byte b : bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+        return buffer.toString();
+    }
+
+    public void incrementNonce() {
+        nonce++;
+    }
+}
---
+++ src/main/java/com/blackhearth/blockchain/block/BlockMiner.java
@@ -0,0 +1,15 @@
+package com.blackhearth.blockchain.block;
+
+public class BlockMiner {
+
+    public Block mineBlock(String data, String previousHash, long timeStamp, int prefix) {
+        Block block = new Block(data, previousHash, timeStamp);
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+        while (!block.getHash().substring(0, prefix).equals(prefixString)) {
+            block.incrementNonce();
+            block.setHash(block.calculateBlockHash());
+        }
+        return block;
+    }
+
+}
