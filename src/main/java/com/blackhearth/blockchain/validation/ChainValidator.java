6b5732d8e0b9f27c524b176fb39585b668316469
Krzysiulek <czarneckikrzysztof97@gmail.com>
2020-10-14 15:42:39 +0200

    jesteśmy dalej o krok

--- src/main/java/com/blackhearth/blockchain/validation/ChainValidator.java
+++ src/main/java/com/blackhearth/blockchain/validation/ChainValidator.java
@@ -2,14 +2,40 @@ package com.blackhearth.blockchain.validation;
 
 import com.blackhearth.blockchain.block.Block;
 import com.blackhearth.blockchain.block.BlockChainRepository;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
 public class ChainValidator implements Validator {
     private BlockChainRepository repository;
 
     @Override
     public boolean isBlockValid(Block block) {
+        return isHashesValid(block) && isProofOfWork();
+    }
+
+    @Override
+    public boolean isTransactionValid(TransactionParams params, Block block) {
+        return isEnoughMoney(params) && isAddressToExists(params) && isSignValid();
+    }
+
+    private boolean isEnoughMoney(TransactionParams params) {
+        String coins = repository.getCoinsFromAddress(params.getAddressFrom()).orElse("0");
+        return Integer.parseInt(coins) >= Integer.parseInt(params.getTransactionMoneyAmount());
+    }
+
+    private boolean isAddressToExists(TransactionParams params) {
+        return repository.getPublicKeyFromAddress(params.getAddressTo()).isPresent();
+    }
+
+    private boolean isSignValid() {
+        // TODO: 13.10.2020
+        //  SignatureGenerator.validateSign z brancha Błażeja
+        return true;
+    }
+
+    private boolean isHashesValid(Block block) {
         int blockPosition = repository.getPositionFromBlockHash(block.getHash()).orElse(0);
         List<Block> blockchain = repository.getBlocksFromPosition(0, blockPosition);
 
@@ -29,23 +55,9 @@ public class ChainValidator implements Validator {
         return !blockchain.isEmpty();
     }
 
-    @Override
-    public boolean isTransactionValid(TransactionParams params, Block block) {
-        return isEnoughMoney(params) && isAddressToExists(params) && isSignValid();
-    }
-
-    private boolean isEnoughMoney(TransactionParams params) {
-        String coins = repository.getCoinsFromAddress(params.getAddressFrom()).orElse("0");
-        return Integer.parseInt(coins) >= Integer.parseInt(params.getTransactionMoneyAmount());
-    }
-
-    private boolean isAddressToExists(TransactionParams params) {
-        return repository.getPublicKeyFromAddress(params.getAddressTo()).isPresent();
-    }
-
-    private boolean isSignValid() {
-        // TODO: 13.10.2020
-        //  SignatureGenerator.validateSign
+    private boolean isProofOfWork() {
+        // TODO: 13.10.2020 czy my to w ogóle musimy robić?
         return true;
     }
+
 }
