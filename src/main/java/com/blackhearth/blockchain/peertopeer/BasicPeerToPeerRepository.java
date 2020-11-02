fec05d050a966c26dba7aecb06d208b207a16364
Krzysiulek <czarneckikrzysztof97@gmail.com>
2020-11-03 00:44:02 +0100

    refactor dla repository v5

--- src/main/java/com/blackhearth/blockchain/peertopeer/BasicPeerToPeerRepository.java
+++ src/main/java/com/blackhearth/blockchain/peertopeer/BasicPeerToPeerRepository.java
@@ -2,22 +2,26 @@ package com.blackhearth.blockchain.peertopeer;
 
 import com.blackhearth.blockchain.node.BlockChainNodeData;
 import com.blackhearth.blockchain.wallet.WalletData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Repository
+@Slf4j
 public class BasicPeerToPeerRepository implements PeerToPeerRepository {
     private Set<BlockChainNodeData> knownNodes = new HashSet<>();
 
     @Override
     public Set<BlockChainNodeData> getNodes() {
+        log.info("Known hosts are: {}", knownNodes.toArray());
         return knownNodes;
     }
 
     @Override
     public void saveNode(BlockChainNodeData data) {
+        log.info("Saving node: {}:{}",data.getIp(), data.getPort());
         knownNodes.add(data);
     }
 
