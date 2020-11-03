10df1fb653f7e1504556c0747a2556ec5fe0bc8c
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2020-11-04 00:26:48 +0100

    Zmieniona implementacja p2p

--- src/main/java/com/blackhearth/blockchain/peertopeer/InterpreterBlockChainCommunication.java
+++ src/main/java/com/blackhearth/blockchain/peertopeer/InterpreterBlockChainCommunication.java
@@ -2,53 +2,51 @@ package com.blackhearth.blockchain.peertopeer;
 
 import com.blackhearth.blockchain.node.BlockChainNodeData;
 import com.blackhearth.blockchain.protocol.interpreter.ProtocolInterpreter;
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Set;
 
 @Slf4j
 @Component
 public class InterpreterBlockChainCommunication implements BlockChainCommunication {
-    private static final int CONNECTION_TIMEOUT = 5000;
-
-    private final Server server;
-    private final Client client;
+    private ServerSocket server;
     private final ProtocolInterpreter interpreter;
     private final BasicPeerToPeerRepository repository;
 
-    public InterpreterBlockChainCommunication(Server server,
-                                              Client client,
-                                              @Lazy ProtocolInterpreter interpreter,
+    public InterpreterBlockChainCommunication(@Lazy ProtocolInterpreter interpreter,
                                               BasicPeerToPeerRepository repository) {
-        this.server = server;
-        this.client = client;
         this.interpreter = interpreter;
         this.repository = repository;
     }
 
     @Override
     public void start(int tcpPort) throws IOException {
-        server.start();
-        server.bind(tcpPort);
+        this.server = new ServerSocket(tcpPort);
         initializeInterpreter();
     }
 
     @Override
-    public void sendTo(String message, String host, int port) throws IOException {
-        client.start();
-        client.connect(CONNECTION_TIMEOUT, host, port);
-        client.sendTCP(new CommunicationObject(message));
-
-        client.close();
+    public void sendTo(String message, String host, int port) {
+        try {
+            log.info("Sending {} to {}:{}", message, host, port);
+            Socket socket = new Socket(host, port);
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(message);
+            oos.close();
+            socket.close();
+            log.info("Sent {} to {}:{}", message, host, port);
+        }
+        catch (Exception e) {
+            log.error("Sending {} to {}:{} failed", message, host, port);
+        }
     }
 
     @Override
@@ -57,39 +55,30 @@ public class InterpreterBlockChainCommunication implements BlockChainCommunicati
     }
 
     @Override
+    @SneakyThrows
     public void closeConnections() {
         server.close();
-        client.close();
-    }
-
-    @PostConstruct
-    private void registerCommunicationClass() {
-        log.info("Registered communictaion class");
-        Kryo serverKryo = server.getKryo();
-        serverKryo.register(CommunicationObject.class);
-
-        Kryo clientKryo = client.getKryo();
-        clientKryo.register(CommunicationObject.class);
     }
 
     private void initializeInterpreter() {
-        server.addListener(new Listener() {
-            @Override
-            public void received (Connection connection, Object object) {
-                if (object instanceof CommunicationObject) {
-                    delegateToInterpretMessage(connection, (CommunicationObject) object);
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket receivedSocket = server.accept();
+                    ObjectInputStream ois = new ObjectInputStream(receivedSocket.getInputStream());
+                    String message = (String) ois.readObject();
+                    delegateToInterpretMessage(receivedSocket.getInetAddress().getHostAddress(), receivedSocket.getPort(), new CommunicationObject(message));
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
                 }
             }
-        });
+        }).start();
     }
 
-    private void delegateToInterpretMessage(Connection connection, CommunicationObject object) {
-        String hostAddress = connection.getRemoteAddressTCP().getAddress().getHostAddress();
-        String port = String.valueOf(connection.getRemoteAddressTCP().getPort());
-        String text = object.getText();
-
-        log.info("Interpreting: {} from {}:{}", text, hostAddress, port);
-        interpreter.interpretMessage(text, hostAddress, port);
+    private void delegateToInterpretMessage(String hostAddress, int port, CommunicationObject object) {
+        String msg = object.getText();
+        log.info("Interpreting: {} from {}:{}", msg, hostAddress, port);
+        interpreter.interpretMessage(msg, hostAddress, String.valueOf(port));
     }
 }
 
