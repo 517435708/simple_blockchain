dabcba845dcd57e50ac00d6d7f6e4bdd4cb1134b
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2020-11-09 01:03:25 +0100

    logi nowe

--- src/main/java/com/blackhearth/blockchain/block/BasicBlockMiner.java
+++ src/main/java/com/blackhearth/blockchain/block/BasicBlockMiner.java
@@ -1,9 +1,12 @@
 package com.blackhearth.blockchain.block;
 
+import com.blackhearth.blockchain.block.repository.BlockChainRepository;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class BasicBlockMiner implements BlockMiner {
 
     private final BlockBuilder blockBuilder;
+    private final BlockChainRepository repository;
     private Block lastMinedBlock;
 
     @Override
@@ -22,6 +26,8 @@ public class BasicBlockMiner implements BlockMiner {
     public void startMining() {
         Block builtBlock = blockBuilder.extractBlock();
         String prefixString = new String(new char[5]).replace('\0', '0');
+        List<Block> longestChain = repository.extractLongestChain();
+        log.info("Longest chain ({}): {}", longestChain.size(), longestChain);
         log.info("Start mining... {}", new Gson().toJson(builtBlock));
         builtBlock.setHash(builtBlock.calculateBlockHash());
         while (!builtBlock.getHash()
--- src/main/java/com/blackhearth/blockchain/protocol/interpreter/BasicProtocolInterpreter.java
+++ src/main/java/com/blackhearth/blockchain/protocol/interpreter/BasicProtocolInterpreter.java
@@ -100,7 +100,7 @@ public class BasicProtocolInterpreter implements ProtocolInterpreter {
     }
 
     private void chainRequest(String address, String port) {
-        Protocol protocol = messageFactory.generateMessages(NODES_RESPONSE);
+        Protocol protocol = messageFactory.generateMessages(CHAIN_RESPONSE);
         p2pService.sendMessageTo(protocol.generateMessage(), address, port);
     }
 
@@ -163,7 +163,10 @@ public class BasicProtocolInterpreter implements ProtocolInterpreter {
     private void addBlock(String value) {
         Block block = new Gson().fromJson(value, Block.class);
         if (validator.isBlockValid(block)) {
+            log.info("BLOCK ADDED");
             blockChainRepository.addToBlockChain(block);
+        } else {
+            log.warn("BLOCK NOT ADDED");
         }
     }
 
